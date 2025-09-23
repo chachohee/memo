789c44461447c40f4817bc0517a51843e7f0184c
chachohee <cxoxee@gmail.com>
2025-09-23 22:49:22 +0900

    2주차 숙제 완료

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -36,5 +36,8 @@ public class MemoController {
         return memoService.deleteMemo(id);
     }
 
-
+    @GetMapping("/memos/contents")
+    public List<MemoResponseDto> getMemosByKeyword(@RequestParam String keyword) { //@RequestParam 생략 가능
+        return memoService.getMemosByKeyword(keyword);
+    }
 }
